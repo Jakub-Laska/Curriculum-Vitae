52d12d9981f60320c991ceeccafb0d6cc90de43c
Klaska <laska.rayl@gmail.com>
2024-09-28 16:24:15 +0200

    add loading screen, change font

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let submitBtn = document.querySelector('#loginBtn');
-let bodyLoading = document.querySelector('#bodyLoading');
-let backgroundVideo = document.querySelector('.backgroundVideo');
-let login = document.querySelector('#login');
-submitBtn.addEventListener('click', loadingScreen);
+//loading screen
+let submitBtn = document.querySelector("#loginBtn");
+submitBtn.addEventListener("click", loadingScreen);
 
 function loadingScreen() {
-    backgroundVideo.style.display = 'none';
-    login.style.display = 'none';
-    bodyLoading.style.display = 'block';
+  let bodyLoading = document.querySelector("#bodyLoading");
+  let backgroundVideo = document.querySelector(".backgroundVideo");
+  let login = document.querySelector("#login");
+  
+  backgroundVideo.style.display = "none";
+  login.style.display = "none";
+  bodyLoading.style.display = "block";
 }
+
+let loginInput = document.querySelector("#loginInput");
+loginInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    loadingScreen();
+  }
+});
