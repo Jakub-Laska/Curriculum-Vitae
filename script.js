6431210fceca6842eb8b53cbd6a0fbbe7ccdffe2
Klaska <laska.rayl@gmail.com>
2024-09-28 14:40:00 +0200

    create loading screen

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hi');
+let submitBtn = document.querySelector('#loginBtn');
+let bodyLoading = document.querySelector('#bodyLoading');
+let backgroundVideo = document.querySelector('.backgroundVideo');
+let login = document.querySelector('#login');
+submitBtn.addEventListener('click', loadingScreen);
+
+function loadingScreen() {
+    backgroundVideo.style.display = 'none';
+    login.style.display = 'none';
+    bodyLoading.style.display = 'block';
+}
