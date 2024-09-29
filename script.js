98fde2dba2c2067c98c7fe176333470053183f33
Klaska <laska.rayl@gmail.com>
2024-09-29 23:28:25 +0200

    add function for switching into a main interface, add wallpaper

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 //loading screen
 let submitBtn = document.querySelector("#loginBtn");
-submitBtn.addEventListener("click", loadingScreen);
+submitBtn.addEventListener("click", showLoadingScreen);
 
-function loadingScreen() {
+function showLoadingScreen() {
   let bodyLoading = document.querySelector("#bodyLoading");
   let backgroundVideo = document.querySelector(".backgroundVideo");
   let login = document.querySelector("#login");
-  
+
   backgroundVideo.style.display = "none";
   login.style.display = "none";
   bodyLoading.style.display = "block";
+  setTimeout(showInterface, 1);
 }
 
 let loginInput = document.querySelector("#loginInput");
 loginInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
-    loadingScreen();
+    showLoadingScreen();
   }
 });
+
+function showInterface() {
+    let bodyInterface = document.querySelector('#interface')
+
+    bodyLoading.style.display = 'none';
+    bodyInterface.style.display = 'block';
+}
